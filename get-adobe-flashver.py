950238c45bec9f8f56857ae2874246784ee0556e
johnraff <github@rafflesnagoya.com>
2016-10-14 15:52:15 +0900

    Use python script to check Adobe version

---
+++ get-adobe-flashver.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2.7
+#
+#    get-adobe-flashver.py: a script to read, from the Adobe website,
+#    the version number of the latest flash plugin for a
+#    particular OS and browser (Linux, Chromium PPAPI by default).
+#
+#    Copyright (C) 2016 Jens John <dev@2ion.de>
+#    Copyright (C) 2016 John Crawley <john@bunsenlabs.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+from bs4 import BeautifulSoup
+import re, sys
+try:
+    # For Python 3
+    from urllib.request import urlopen, URLError
+except ImportError:
+    # For Python 2
+    from urllib2 import urlopen, URLError
+
+# These regexes will be *searched* for in the html table text.
+# Use ^ and $ as necessary.
+OS_regex = 'Linux'
+TYPE_regex = 'Chromium.* PPAPI'
+
+adobe_url = 'https://www.adobe.com/software/flash/about/'
+ca_certs_path = '/etc/ssl/certs' # ca-certificates on Debian Jessie
+
+def err_exit(*msgs):
+    print('\n'.join(msgs), file=sys.stderr)
+    sys.exit(1)
+
+class Column:
+    OS = 0
+    TYPE = 1
+    VERSION = 2
+
+def table2matrix(t):
+    rows = t.find_all("tr")
+    m = [ [] for _ in range(len(rows)) ]
+    i = 0
+    for tr in rows:
+        for td in tr.find_all("td"):
+            if 'rowspan' in td.attrs:
+                rowsp = int(td["rowspan"])
+            else:
+                rowsp = 1
+            for j in range(i, i + int(rowsp)):
+                m[j].append(td.get_text(separator=' ', strip=True))
+        i += 1
+    return [ x for x in m if x ]
+
+def latest_version():
+    try:
+        body = urlopen(adobe_url, capath=ca_certs_path).read()
+    except URLError as e:
+        err_exit('URLError: Bad URL or certs path?', str(e.reason))
+    soup = BeautifulSoup(body, 'html.parser')
+    matrix = table2matrix(soup.find('table', { 'class': 'data-bordered' }))
+    version, = [x[Column.VERSION] for x in matrix if re.search(OS_regex, x[Column.OS]) and re.search(TYPE_regex, x[Column.TYPE])]
+    return version
+
+try:
+    print(latest_version())
+except ValueError:
+    err_exit('Parsing error: regular expressions wrong?')
